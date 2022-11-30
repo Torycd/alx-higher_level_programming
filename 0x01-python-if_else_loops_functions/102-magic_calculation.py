816104c4997712fd206c08f4a748f65fd2a8ac12
torycd <vicidundun@gmail.com>
2022-11-29 19:18:33 -0800

    commit

---
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# Author - Idundun Victoru
+
+def magic_calculation(a, b, c):
+    """Math bytecode provided by Holberton cycle"""
+    if a < b:
+        return(c)
+    if c > b:
+        return (a + b)
+    return (a*b -c)
